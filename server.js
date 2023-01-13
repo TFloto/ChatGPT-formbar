7bb2596ebdb7ad275f43941a653126cfdc811fc0
Trey <90394867+TFloto@users.noreply.github.com>
2023-01-13 10:58:24 -0500

    added header, pointless footer

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const ejs = require('ejs');
 const app = express();
+const os = require('os');
+let ip = "";
+
+const interfaces = os.networkInterfaces();
+Object.keys(interfaces).forEach(function (interfaceName) {
+  interfaces[interfaceName].forEach(function (interface) {
+    if (interface.family === 'IPv4' && !interface.internal) {
+      ip = interface.address;
+    }
+  });
+});
+
 
 app.set('view engine', 'ejs'); // set the view engine to ejs
 app.use(express.static('views')); // serve the views directory as static files
