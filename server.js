d83e07eb52a82b3c7d504d6a802a2f4df29097ce
Skayote <90394867+TFloto@users.noreply.github.com>
2023-01-16 18:27:25 -0500

    guest accounts are now coherent

--- server.js
+++ server.js
@@ -2,10 +2,20 @@ const express = require('express');
 const ejs = require('ejs');
 const app = express();
 
-let visitors = [];
-
 app.set('view engine', 'ejs'); // set the view engine to ejs
 app.use(express.static('views')); // serve the views directory as static files
+
+let guestNames = new Map();
+let visitors = new Set();
+
+app.use((req, res, next) => {
+  let ip = req.connection.remoteAddress;
+  if(!guestNames.has(ip)){
+    guestNames.set(ip, `Guest ${Math.floor(Math.random() * 900000) + 100000}`);
+  }
+  visitors.add(ip);
+  next();
+});
 
 app.get('/', (req, res) => {
   res.render('index.ejs', { menu: [] });
@@ -21,8 +31,16 @@ app.get('/tutd', (req, res) => {
 });
 
 app.get('/users', (req, res) => {
-  visitors.push(req.connection.remoteAddress);
-  res.render('users.ejs', { visitors: visitors });
+  let ipToName = new Map();
+  guestNames.forEach((value, key) => {
+    ipToName.set(key, value)
+  });
+  res.render('users.ejs', { visitors: ipToName });
+});
+
+
+app.get('/login', (req, res) => {
+  res.render('login.ejs');
 });
 
 app.listen(8000, () => {
