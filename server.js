93c99a883832da8aafe1e3e62a366eafa89fac4e
Trey <90394867+TFloto@users.noreply.github.com>
2023-01-17 14:10:43 -0500

    virtualbar slightly works now

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ let visitors = new Set();
 app.use((req, res, next) => {
   let ip = req.connection.remoteAddress;
   if(!guestNames.has(ip)){
-    guestNames.set(ip, `Guest ${Math.floor(Math.random() * 900000) + 100000}`);
+    guestNames.set(ip, {name: `Guest ${Math.floor(Math.random() * 900000) + 100000}`, thumb: ""});
   }
   visitors.add(ip);
   next();
@@ -23,21 +23,30 @@ app.get('/', (req, res) => {
 
 app.get('/tutd', (req, res) => {
   res.render('tutd.ejs', { buttons: [
-    { name: 'upthumb', url: '/upthumb' },
-    { name: 'wigglethumb', url: '/wigglethumb' },
-    { name: 'downthumb', url: '/downthumb' },
+    { name: 'upthumb', url: '/tutd/upthumb' },
     { name: 'removethumb', url: '/removethumb' }
   ], visitors: guestNames });
 });
 
+app.get('/tutd/upthumb', (req, res) => {
+  let ip = req.connection.remoteAddress;
+  // update the guestNames map with the thumb status
+  guestNames.set(ip, {name: guestNames.get(ip).name, thumb: "up"});
+  res.render('tutd.ejs', { buttons: [
+  { name: 'upthumb', url: '/upthumb' },
+  { name: 'removethumb', url: '/removethumb' }
+  ], visitors: guestNames });
+});
+
+
 app.get('/users', (req, res) => {
-  res.render('users.ejs', { visitors: guestNames });
+res.render('users.ejs', { visitors: guestNames });
 });
 
 app.get('/login', (req, res) => {
   res.render('login.ejs', { visitors: guestNames });
-});
-
-app.listen(8000, () => {
+  });
+  
+  app.listen(8000, () => {
   console.log('Server listening on http://localhost:8000');
-});
+  });
